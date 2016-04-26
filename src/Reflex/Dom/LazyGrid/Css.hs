31c28d7d52f31a5af24b1aad5a730cac2e398844
Maciej Kazulak <kazulakm@gmail.com>
2016-04-26 21:59:01 +0200

    wip

--- src/Reflex/Dom/LazyGrid/Css.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE TemplateHaskell #-}
-module Reflex.Dom.LazyGrid.Css where
-
-import Data.ByteString
-import Data.FileEmbed (embedFile)
-
-lazyGridCss :: ByteString
-lazyGridCss = $(embedFile "grid.css")
